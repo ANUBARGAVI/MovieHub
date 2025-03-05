fb8a54cf7216514c4f2a7a2f7bf9f64834aafa1d
ANUBARGAVI <nannaamma1319@gmail.com>
2025-03-05 21:46:54 +0530

    generated npm  build production

--- MovieHub/vite.config.js
+++ MovieHub/vite.config.js
@@ -1,24 +1,12 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     react(),
-//     tailwindcss(),
-//   ],
-// })
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/", // Ensures correct asset paths in Vercel
-  build: {
-    outDir: "dist", // Ensures Vite builds into the correct folder
-  },
-});
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
 
